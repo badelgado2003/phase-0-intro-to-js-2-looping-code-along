326b85f8a740a0e575a35b05fc921341ad4b1bec
Braydon <BADelgado2003@outlook.com>
2022-11-29 20:52:39 -0500

    commit tree d67800fefd816e13af97970aa77643e5f789089c

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solutions in this file
+function writeCards(names, surprise) {
+    let array = [names];
+    return array.forEach(msg => `Thank you, ${msg[0]}, for the wonderful ${surprise} gift!`)
+}
